922b3bfe69475d8091cc05de3e8113b322f597c8
windmill312 <windmill5667@gmail.com>
2019-03-30 13:15:49 +0300

    Initial commit

---
+++ src/main/java/com/sychev/events/model/entity/EventEntity.java
@@ -0,0 +1,16 @@
+/*package com.sychev.events.model.entity;
+
+import javax.persistence.Entity;
+import java.time.Instant;
+
+@Entity
+public class EventEntity {
+    private Integer id;
+    private String name;
+    private String description;
+    private Instant dateFrom;
+    private Instant dateTill;
+    private Instant timeFrom;
+    private Instant timeTill;
+
+}*/
